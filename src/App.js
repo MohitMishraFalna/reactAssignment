a8a2e3831846f45addbe5d49000bb5eb734ed4f4
Chinmay Mishra <chinmay@ezdatechnology.com>
2022-09-02 12:00:03 +0530

    make it better

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
+import Welcome from "./pages/welcome/Welcome";
+import "./app.scss";
+import Home from "./pages/home/Home";
+import Planning from "./pages/planning/Planning";
+import Congratulation from "./pages/congratulation/Congratulation";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
+    <div className="app">
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Welcome/>
+          </Route>
+          <Route path="/home">
+            <Home/>
+          </Route>
+          <Route path="/planning">
+            <Planning />
+          </Route>
+          <Route path="/congratulation">
+            <Congratulation />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
